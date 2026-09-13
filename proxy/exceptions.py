18b8e7ac152a02ddea23a07c0642169841be19a0
Olga Zhivaeva <olga.zhiv@yandex.ru>
2026-09-13 11:43:15 +0300

    Transfer processing of TE+CL and broken Content-Length, chunked trailers and early EOF upstream from feature/timeouts branch

--- proxy/exceptions.py
+++ proxy/exceptions.py
@@ -11,3 +11,33 @@ class HttpRequestError(ProxyBaseError):
 class UpstreamError(ProxyBaseError):
     """Ошибка при работе с апстримом (502 Bad Gateway)."""
     pass
+
+
+class ClientReadTimeoutError(ProxyBaseError):
+    """Таймаут передачи клиентом запроса или тела (408 Request Timeout)."""
+    pass
+
+
+class ClientWriteTimeoutError(ProxyBaseError):
+    """Таймаут при отправке ответа клиенту."""
+    pass
+
+
+class UpstreamTimeoutError(ProxyBaseError):
+    """Базовый таймаут для апстрима (504 Gateway Timeout)."""
+    pass
+
+
+class UpstreamReadTimeoutError(UpstreamTimeoutError):
+    """Таймаут чтения ответа от апстрима (504 Gateway Timeout)."""
+    pass
+
+
+class UpstreamWriteTimeoutError(UpstreamTimeoutError):
+    """Таймаут отправки запроса на апстрим (504 Gateway Timeout)."""
+    pass
+
+
+class UpstreamConnectTimeoutError(UpstreamTimeoutError):
+    """Таймаут подключения к апстриму (504 Gateway Timeout)."""
+    pass
